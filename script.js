b717ad9acc29ff503f423735e518f0fea4433a29
unknown <farheenirfan12nov@gmail.com git config --global user.name Farheenirfan123>
2024-01-29 04:46:44 -0800

    string method

---
+++ script.js
@@ -0,0 +1,36 @@
+// let myArrray =[
+//     ["aliza", "20", "true"],
+//     ["aisha", "22", "false"],
+//     ["zara", "22","true"]
+    
+
+// ]
+    
+//       for(i=0; i<myArrray.length; i++){
+//         document.write(`persons ${i} <br> <br>`)
+
+//         for(c=0; c<myArrray.length; c++){
+//         document.write(`-- ${myArrray[i][c]} <br> <br>`)
+//         }
+
+//       } 
+    let x = prompt()
+      switch(x){
+        case x: "+"
+        console.log(x ,"addition");
+        break;
+
+        case x: "-"
+        console.log(x ,"subtraction");
+        break;
+
+        case x: "*"
+        console.log(x ,"multiplication");
+        break;
+
+        case x: "/"
+        console.log(x ,"division");
+        break;
+
+
+      }
